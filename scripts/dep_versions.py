f68b9130bc76ec76d960f3f48eb174b565cf1035
Jarrad Whitaker <akdor1154 at gmail>
2023-01-27 17:36:48 +1100

    update deps script

---
+++ scripts/dep_versions.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import subprocess
+import functools
+import contextlib
+import os
+import sys
+import textwrap
+
+run = functools.partial(subprocess.run, check=True, encoding='utf-8')
+eprint = functools.partial(print, file=sys.stderr)
+
+@contextlib.contextmanager
+def cd(dir: str):
+    currentDir = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield
+    finally:
+        os.chdir(currentDir)
+
+def main():
+    run(['git', 'checkout', 'main'])
+
+    with cd('./ubuntu_22.04/01-deps'):
+        run(['make', 'check-deps'])
+
+        with open('versions.mk') as f:
+            old_versions = f.read()
+
+        with open('versions_new.mk') as f:
+            new_versions = f.read()
+
+        if old_versions == new_versions:
+            eprint('No updates!')
+            sys.exit(0)
+        
+        eprint('Updates found!')
+
+
+    BRANCH_NAME = 'auto-versions'
+    run(['git', 'checkout', '-B', BRANCH_NAME])
+
+    commit_new_versions(new_versions)
+    raise_pr(BRANCH_NAME)
+
+    exit(42)
+    
+
+def commit_new_versions(versions_text):
+    MSG = f'Bump dependencies\n\n{versions_text}'
+    CHANGELOG_FILE = 'ubuntu_22.04/03-package/changelog'
+    VERSIONS_FILE = 'ubuntu_22.04/01-deps/versions.mk'
+    with open(VERSIONS_FILE, 'wt') as f:
+        f.write(versions_text)
+    run(['dch', '-i', '-U', '-c', CHANGELOG_FILE, '-b', MSG], env={**os.environ, 'EMAIL':'akdor1154@noreply.users.github.com'})
+    run(['git', 'add', VERSIONS_FILE, CHANGELOG_FILE])
+    run(['git', 'commit', '-m', MSG, '--', VERSIONS_FILE, CHANGELOG_FILE])
+
+def raise_pr(branch_name: str):
+    run(['git', 'push', '-u', 'origin', '--force', f'{branch_name}:{branch_name}'])
+    run(['gh', 'pr', 'create', '--fill', '--assignee', 'akdor1154'])
+
+if __name__ == '__main__':
+    main()
---
+++ ubuntu_22.04/01-deps/query-deps.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import subprocess
+import functools
+import sys
+import dataclasses
+from typing import *
+
+run = functools.partial(subprocess.run, check=True, encoding='utf-8')
+eprint = functools.partial(print, file=sys.stderr)
+
+def get_available_version(package_name: str, dist_name: str):
+    output = run(['apt-cache', 'madison', package_name], capture_output=True).stdout.splitlines()
+    matching = [line for line in output if dist_name in line.split(' ') and 'Sources' in line]
+    if len(matching) < 1:
+        eprint(output)
+        raise Exception(f'Package {package_name} not found in {dist_name}!')
+    elif len(matching) > 1:
+        eprint(output)
+        raise Exception(f'Package {package_name} found multiple versions, aborting!')
+
+    name, version, *stuff = map(str.strip, matching[0].split('|'))
+
+    eprint(f'found {package_name}={version}')
+    return version
+
+@dataclasses.dataclass(frozen=True)
+class Dep:
+    dep_name: str
+    package_name: str
+    dist: str
+
+DEPS: List[Dep] = [
+    Dep('WAYLAND_VERSION', 'wayland', 'kinetic/main'),
+    Dep('LIBDRM_VERSION', 'libdrm', 'kinetic/main'),
+    Dep('WAYLAND_PROTOCOLS_VERSION', 'wayland-protocols', 'unstable/main'),
+    Dep('HWDATA_VERSION', 'hwdata', 'unstable/main'),
+    Dep('VULKAN_LOADER_VERSION', 'vulkan-loader', 'unstable/main')
+]
+
+for dep in DEPS:
+    version = get_available_version(dep.package_name, dep.dist)
+    print(f'{dep.dep_name}={version}')
