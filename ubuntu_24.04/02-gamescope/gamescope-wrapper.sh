f6e56d015ef97179182a14691127730cb6a6bc02
Jarrad Whitaker <akdor1154@gmail.com>
2025-06-09 12:54:13 +1000

    enable realtime / remove the wrapper script

--- ubuntu_24.04/02-gamescope/gamescope-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PACKAGE_PATH="$( cd -- "$(dirname "$(realpath "$0" )" )" >/dev/null 2>&1 ; cd .. ; pwd -P )"
-ME="$( basename "$0" )" # e.g. "gamescope", "gamescopereaper"
-
-exec /usr/lib64/ld-linux-x86-64.so.2 \
-    --library-path "${PACKAGE_PATH}/lib/x86_64-linux-gnu" \
-    "${PACKAGE_PATH}/bin/${ME}.real" \
-    "$@"
